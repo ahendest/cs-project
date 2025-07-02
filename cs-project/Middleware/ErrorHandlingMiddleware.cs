590a8f373a3554d2dfb458834949e3fa3c8edf9b
ahendest <goz.m.abdurrahman23@stud.rau.ro>
2025-07-02 17:09:19 +0300

    add global error handler

---
+++ cs-project/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace cs_project.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                var problemDetails = new
+                {
+                    status = context.Response.StatusCode,
+                    title = "An unexpected error occurred.",
+                    detail = ex.Message
+#if DEBUG
+                    ,
+                    stackTrace = ex.StackTrace
+#endif
+
+                };
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(problemDetails, options));
+            }
+        }
+    }
+}
