35f225a442d9761f8723eea128d1c763af82b4fe
ahendest <goz.m.abdurrahman23@stud.rau.ro>
2025-07-02 13:19:39 +0300

    add validators and it's tests

--- cs-project.Core/DTOs/TransactionsCreateDTO.cs
+++ cs-project.Core/DTOs/TransactionsCreateDTO.cs
@@ -1,9 +1,4 @@
 ﻿using cs_project.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace cs_project.Core.DTOs
 {
@@ -14,7 +9,6 @@ namespace cs_project.Core.DTOs
         public double PricePerLiter { get; set; }
         public double TotalPrice { get; set; }
         public DateTime Timestamp { get; set; }
-
         public Pump? Pump { get; set; }
     }
 }
---
+++ cs-project/Validators/TransactionCreateDTOValidator.cs
@@ -0,0 +1,27 @@
+﻿using cs_project.Core.DTOs;
+using FluentValidation;
+
+namespace cs_project.Validators
+{
+    public class TransactionCreateDTOValidator : AbstractValidator<TransactionsCreateDTO>
+    {
+        public TransactionCreateDTOValidator()
+        {
+            RuleFor(x => x.PumpId)
+                .GreaterThan(0).WithMessage("PumpId must be a positive integer.");
+
+            RuleFor(x => x.Liters)
+                .GreaterThan(0).WithMessage("Liters must be greater than 0.");
+
+            RuleFor(x => x.PricePerLiter)
+                .GreaterThan(0).WithMessage("PricePerLiter must be greater than 0.");
+
+            RuleFor(x => x.TotalPrice)
+                .GreaterThanOrEqualTo(0).WithMessage("TotalPrice must be 0 or greater.");
+
+            RuleFor(x => x.Timestamp)
+                .NotEmpty().WithMessage("Timestamp is required.")
+                .LessThanOrEqualTo(_ => DateTime.UtcNow).WithMessage("Timestamp cannot be in the future.");
+        }
+    }
+}
